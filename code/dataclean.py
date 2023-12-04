e8741eafe205ac9f91a6ca25640a108808b3795a
JacobMcGill <jacobpmcgill97@gmail.com>
2023-12-04 10:08:19 -0600

    fixing branch

--- code/dataclean.py
+++ code/dataclean.py
@@ -6,12 +6,16 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+in_path = os.path.join("data", "Austin_Hotel_Reviews_with_latitudelongitude_.csv")
+out_dir = "artifacts"
+output_path = os.path.join(out_dir, "cleaned_hotel_data.csv")
+
 #reomve reduntant columns from csv
-df1 = pd.read_csv('data/Austin_Hotel_Reviews_with_latitudelongitude_.csv')
+df1 = pd.read_csv(in_path)
 
 df_columns_cut_off = df1.drop(['query', 'review_pagination_id','author_link', 'author_image','review_img_url','review_img_urls','review_photo_ids','owner_answer_timestamp_datetime_utc','review_link','review_datetime_utc','reviews_id','review_questions_Rooms','review_questions_Service','review_questions_Location','review_questions','review_questions_Trip type','review_questions_Travel group','review_questions_Hotel highlights','review_questions_Safety','review_questions_Noteworthy details','review_questions_None','review_questions_Nearby activities','review_questions_Safety','review_questions_Walkability','review_questions_Food & drinks','review_questions_Wi-fi'],axis=1)
 
-df_columns_cut_off['Google Reivews'] = df_columns_cut_off['review_text']
+df_columns_cut_off['Google Reviews'] = df_columns_cut_off['review_text']
 
 #remove stopwords from 'Google Reviews'
 nltk.download('punkt')
@@ -23,5 +27,5 @@ def remove_stopwords(text):
     return ' '.join([word for word in words if word.lower() not in stop_words])
 
 df = df_columns_cut_off
-df['Google Reivews'] = df['Google Reivews'].astype(str).apply(remove_stopwords)
-print(df['Google Reivews'])
+df['Google Reviews'] = df['Google Reviews'].astype(str).apply(remove_stopwords)
+df.to_csv(output_path, index=False, mode ="w")
